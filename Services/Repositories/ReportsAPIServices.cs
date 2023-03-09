7888fc87e018f37a4d279bd1107a37daa1317b8e
DucViet519 <ducviet519@gmail.com>
2023-03-09 10:45:39 +0700

    ok

---
+++ Services/Repositories/ReportsAPIServices.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTools.Services.Interface;
+
+namespace WebTools.Services.Repositories
+{
+    public class ReportsAPIServices : IReportsAPIServices, IDisposable
+    {
+        private readonly RestClient _client;
+        //public
+
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
